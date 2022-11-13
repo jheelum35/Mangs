f536010b0503d2046750aef8d2a07fcfe0337938
jheelum35 <ggithub7@gmail.com>
2022-11-13 14:00:39 +0530

    commit

--- Mango/src/test/java/org/mango/Mango/test/APITest.java
+++ Mango/src/test/java/org/mango/Mango/test/APITest.java
@@ -7,7 +7,12 @@ public class APITest {
 	@Test
 	public void Test1()
 	{
-		System.out.println("Test1");
+		System.out.println("Test1------->H1");
 	}
 
+	@Test
+	public void Test2()
+	{
+		System.out.println("Test1------->H2");
+	}
 }
